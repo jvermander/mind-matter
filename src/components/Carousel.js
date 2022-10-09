989daef01388bf2a6d8b69b7fbbf5f05fb0c8e5c
Joe Vermander <vermander1@protonmail.com>
2022-10-09 17:41:27 -0500

    Pre-flexbox carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,24 @@
-import React, { Children, useState } from 'react'
+import React, { Children, useState, useEffect } from 'react'
 import 'styles/Carousel.css'
 
 function Carousel({ children }) {
   const [selected, setSelected] = useState(0)
-  const onNavButtonClick = (index) => {setSelected(index)}
+  const onNavButtonClick = (index) => {
+    setSelected(index)
+  }
+
+  useEffect(() => {
+    console.log(selected)
+  }, [selected])
+
+  const onRightBtnClick = () => {
+    setSelected((selected+1) % Children.count(children))
+  }
+
+  const onLeftBtnClick = () => {
+    var count = Children.count(children)
+    setSelected((selected-1 + count) % count)
+  }
 
   const navButtons = 
     Children.map(children, (child, index) => (
@@ -14,7 +29,6 @@ function Carousel({ children }) {
     Children.map(children, (child, index) => (
       React.cloneElement(child, {selected: selected === index} )))
 
-
   return (
     <div className="carousel-ctn">
       <div className="carousel-display">
@@ -22,6 +36,8 @@ function Carousel({ children }) {
       </div>
       <div className="carousel-nav">
         {navButtons}
+        <button onClick={onLeftBtnClick}>Left</button>
+        <button onClick={onRightBtnClick}>Right</button>
       </div>
     </div>
   )
